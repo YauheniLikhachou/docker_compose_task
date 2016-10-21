40054218bd6d8ce9b4df90a03041f3e98491fead
YauheniLikhachou <Yauheni_Likhachou2@epam.com>
2016-10-21 10:36:53 +0300

    Fised bug for upload job into jenkins first time

--- resource/resource_jjb/jjb.sh
+++ resource/resource_jjb/jjb.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sleep 30
-jenkins-jobs --conf /opt/resource_jjb/jjb.ini update /opt/resource_jjb/boxfuse-sample-java-war-hello-build.yaml
+until curl --output /dev/null --silent --head --fail http://jenkins:8080
+do
+sleep 1
+printf 'Wait...'
+done && jenkins-jobs --conf /opt/resource_jjb/jjb.ini update /opt/resource_jjb/boxfuse-sample-java-war-hello-build.yaml
